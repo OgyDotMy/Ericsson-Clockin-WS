ad06ae53d39b4764ccc4134e3a755b53b2bd37b7
OgyDotMy <gulthaj92@gmail.com>
2022-09-26 00:35:06 +0800

    update isset

--- view/home.php
+++ view/home.php
@@ -22,21 +22,39 @@ date_default_timezone_set("Asia/Kuala_Lumpur");
         }else{
             $cleantt=$nott;
         }
+        if(empty($pictt)){
+            $errorpictt = "PICTT need to input";
+        }else{
+            $cleanpictt=$pictt;
+        }
+        if(empty($ttteam)){
+            $errorttteam = "No TT need to input";
+        }else{
+            $cleanttteam=$ttteam;
+        }
 
+    
+
+        if(isset($cleansite) && isset ($cleantt) && isset($cleanpictt) && isset($cleanttteam) ){
+            header("location:https://api.whatsapp.com/send?phone=$urnumberphone//&text=Hi%20NOC,i%20want%20to%20login/out%0a%0aProject : 5G_DNB%0a%0aSITE%20:%20$cleansite%0a%0aNOC TT NO%20:%20$nott%0a%0aPIC DNB : %0a%0aPIC TT : $pictt%0a%0aTT TEAM : $ttteam%0a%0aSOW : $sow%0a%0aDATE : $date%0a%0aTIME IN : $time%0a%0aTime OUT : %0a%0aRemark : $remark");
+        }else{
+            header("location:https://google.com");
+        }
+
+    }
         // echo $siteid.$nott.$pictt.$ttteam.$sow;
         // header("location:http://checkingalarm.test/view/send.php");
-        header("location:https://api.whatsapp.com/send?phone=$urnumberphone//&text=Hi%20NOC,i%20want%20to%20login/out%0a%0aProject : 5G_DNB%0a%0aSITE%20:%20$cleansite%0a%0aNOC TT NO%20:%20$nott%0a%0aPIC DNB : %0a%0aPIC TT : $pictt%0a%0aTT TEAM : $ttteam%0a%0aSOW : $sow%0a%0aDATE : $date%0a%0aTIME IN : $time%0a%0aTime OUT : %0a%0aRemark : $remark");
         // header("locationL/index.php?url-home");
         // exit();
         // $urlweb="https://wa.me/60176127934?text=site&id'.$siteid.";
 
-    }else{
-        // header("location:http://checkingalarm.test/index.php?url=1");
-        // exit();
-        // $verser=$_SERVER['SERVER_NAME']
-        // header("location:home.php");
+    // }else{
+    //     // header("location:http://checkingalarm.test/index.php?url=1");
+    //     // exit();
+    //     // $verser=$_SERVER['SERVER_NAME']
+    //     // header("location:home.php");
 
-    }
+    // }
 
 // include("view/home.php");
 
