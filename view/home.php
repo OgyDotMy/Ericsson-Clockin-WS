239f6de920187946961a50fc4be4858196ccac6d
OgyDotMy <gulthaj92@gmail.com>
2023-07-20 23:23:27 +0800

    update css

--- view/home.php
+++ view/home.php
@@ -69,7 +69,7 @@ date_default_timezone_set("Asia/Kuala_Lumpur");
     <meta name="description" content="Ericcson ClockIN ZO SITE">
     <title>CLOCKIN POST SYSTEM</title>
     <link rel="shortcut icon" href="https://previews.123rf.com/images/subhanbaghirov/subhanbaghirov1909/subhanbaghirov190900042/130565753-5g-icon-network-coverage-area-simple-flat-style-symbol-.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="<?php get_css("style.css") ?>">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
